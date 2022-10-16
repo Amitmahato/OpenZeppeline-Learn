f739c29edac34af84458887ab17c95aa166876d1
Amit Mahato <amit643.mahato@gmail.com>
2022-10-16 15:59:36 +0545

    5.3 Performing complex assertions with OpenZeppeline's Test Helpers - define & compare bignumbers - define a test to check for the emitted event - define a test to check the transaction reverts when non-owner tries to store a value

--- test/Box.test.js
+++ test/Box.test.js
@@ -3,27 +3,45 @@
 // Load Dependencies
 const { expect } = require("chai");
 
+// Import utilities from OpenZeppelin Test Helpers
+const { BN, expectEvent, expectRevert } = require("@openzeppelin/test-helpers");
+
 // Load compiled artifacts
 const Box = artifacts.require("Box");
 
 // Start test block
-contract("Box", () => {
+contract("Box", ([owner, other]) => {
+  const value = new BN("8545");
+
   beforeEach(async () => {
     // Deploy a new Box contract for each test
-    this.box = await Box.new();
+    this.box = await Box.new({ from: owner });
   });
 
   it("retrieve returns a value previously stored", async () => {
-    const value = 8545;
     // Store a value
-    await this.box.store(value);
+    await this.box.store(value, { from: owner });
 
     // Test if the returned value is the same one
-    // Note that we need to use strings to compare the 256 bit integers
-    const storedValue = parseInt((await this.box.retrieve()).toString());
-    expect(storedValue).to.equal(
+    const storedValue = await this.box.retrieve();
+    expect(storedValue).to.be.bignumber.equal(
       value,
       "Stored & Retrieved values mis-matched"
+    );
+  });
+
+  it("storing a value emits an event", async () => {
+    const receipt = await this.box.store(value, { from: owner });
+
+    // Test that a ValueChanged event was emitted with the new value
+    expectEvent(receipt, "ValueChanged", { value });
+  });
+
+  it("should not allow non owner to store value", async () => {
+    // Test a transaction reverts - expects the reason of the revert to match
+    await expectRevert(
+      this.box.store(value, { from: other }),
+      "Ownable: caller is not the owner"
     );
   });
 });
