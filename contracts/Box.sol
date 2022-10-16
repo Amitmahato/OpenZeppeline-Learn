a5ec7dc1870e42ae9680376d123c735f723ef4d1
Amit Mahato <amit643.mahato@gmail.com>
2022-10-16 13:58:31 +0545

    2.4 Importing OpenZeppelin Contracts - install dependency @openzeppelin/contracts - replace the implementation for access control defined by Auth contract by inheriting OpenZeppelin's Ownable contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,25 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+contract Box is Ownable {
     uint256 private _value;
-    Auth private _auth;
 
     // This event is emitted when the stored value changes
     event ValueChanged(uint256 value);
 
-    constructor() {
-        _auth = new Auth(msg.sender);
-    }
-
     // Stores a new value in the contract
-    function store(uint256 value) public {
-        // Require that the caller is registered as an administrator in Auth
-        require(_auth.isAdministrator(msg.sender), "Unauthorized");
-
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
